ec83725c9e7d48ec3dac62dbaa41363fcfed08ee
Borislav Iliev <borislav.iliev2002@gmail.com>
2023-08-21 15:53:59 +0300

    Added unit tests for Comment Service.

--- src/test/java/bg/softuni/kickboxing/service/CommentServiceTest.java
+++ src/test/java/bg/softuni/kickboxing/service/CommentServiceTest.java
@@ -1,9 +1,15 @@
+package bg.softuni.kickboxing.service;
+
+import bg.softuni.kickboxing.model.dto.comment.AddCommentDTO;
+import bg.softuni.kickboxing.model.dto.comment.CommentDTO;
 import bg.softuni.kickboxing.model.entity.CommentEntity;
 import bg.softuni.kickboxing.model.entity.PostEntity;
 import bg.softuni.kickboxing.model.entity.UserEntity;
 import bg.softuni.kickboxing.model.entity.UserRoleEntity;
 import bg.softuni.kickboxing.model.enums.PostCategoryEnum;
 import bg.softuni.kickboxing.model.enums.UserRoleEnum;
+import bg.softuni.kickboxing.model.exception.ObjectNotFoundException;
+import bg.softuni.kickboxing.model.user.GlovesOnPointUserDetails;
 import bg.softuni.kickboxing.repository.CommentRepository;
 import bg.softuni.kickboxing.repository.PostRepository;
 import bg.softuni.kickboxing.repository.UserRepository;
@@ -13,24 +19,35 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.lang.reflect.Executable;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CommentServiceTest {
 
-    @Autowired
+    @InjectMocks
     private CommentService toTest;
 
-    @Autowired
+    @Mock
     private ModelMapper mapper;
 
     @Mock
@@ -42,22 +59,103 @@ public class CommentServiceTest {
     @Mock
     private PostRepository postRepository;
 
-    @BeforeEach
-    public void setUp() {
-        this.toTest = new CommentService(this.commentRepository,
-                this.mapper, this.postRepository, this.userRepository);
-    }
+    private Pageable pageable;
+
+    @Mock
+    private GlovesOnPointUserDetails userDetails;
+
+//    @BeforeEach
+//    public void setUp() {
+//        this.toTest = new CommentService(this.commentRepository,
+//                this.mapper, this.postRepository, this.userRepository);
+//    }
 
     @AfterEach
     public void tearDown() {
-        this.userRepository.deleteAll();
-        this.commentRepository.deleteAll();
-        this.postRepository.deleteAll();
-        initComment();
+        reset(this.userRepository);
+        reset(this.commentRepository);
+        reset(this.postRepository);
+        reset(this.mapper);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"ADMIN", "MODERATOR"})
+    void testAddComment_ShouldAddCommentAndSetIsApprovedToTrue_WhenUserIsAdmin(String userRole) {
+        AddCommentDTO addCommentDTO = initAddCommentDto();
+        CommentEntity comment = initComment();
+        comment.setApproved(true);
+
+        when(this.mapper.map(addCommentDTO, CommentEntity.class))
+                .thenReturn(comment);
+
+        PostEntity post = initPost();
+        when(this.postRepository.findById(1L))
+                .thenReturn(Optional.of(post));
+
+        UserEntity user = initUser();
+        when(this.userDetails.getUsername())
+                .thenReturn("Username");
+        when(this.userDetails.getRole()).
+                thenReturn(userRole);
+
+        when(this.userRepository.findByUsername(this.userDetails.getUsername()))
+                .thenReturn(Optional.of(user));
+
+        when(this.commentRepository.save(comment))
+                .thenReturn(comment);
+
+        this.toTest.addComment(1L, addCommentDTO, userDetails);
+
+        verify(this.commentRepository, times(1)).save(comment);
+        assertTrue(comment.isApproved());
     }
 
     @Test
-    public void testApprovePostShouldSetApprovedToTrue() {
+    void testAddComment_ShouldAddCommentAndSetIsApprovedToFalse_WhenUserIsANormalUser() {
+        AddCommentDTO addCommentDTO = initAddCommentDto();
+        CommentEntity comment = initComment();
+
+        when(this.mapper.map(addCommentDTO, CommentEntity.class))
+                .thenReturn(comment);
+
+        PostEntity post = initPost();
+        when(this.postRepository.findById(1L))
+                .thenReturn(Optional.of(post));
+
+        UserEntity user = initUser();
+        when(this.userDetails.getUsername())
+                .thenReturn("Username");
+        when(this.userDetails.getRole()).
+                thenReturn("USER");
+
+        when(this.userRepository.findByUsername(this.userDetails.getUsername()))
+                .thenReturn(Optional.of(user));
+
+        when(this.commentRepository.save(comment))
+                .thenReturn(comment);
+
+        this.toTest.addComment(1L, addCommentDTO, userDetails);
+
+        verify(this.commentRepository, times(1)).save(comment);
+        assertFalse(comment.isApproved());
+    }
+
+    @Test
+    void testGetAllNotApprovedCommentsOrderedByDateDesc_ShouldReturnListOfCommentDto() {
+        List<CommentDTO> comments = List.of(initCommentDto(), initCommentDto());
+        PageImpl<CommentDTO> expectedCommentsPage = new PageImpl<>(comments);
+
+        when(this.commentRepository.getAllNotApprovedCommentsOrderedByCreatedOnDesc(this.pageable))
+                .thenReturn(expectedCommentsPage);
+
+        Page<CommentDTO> actualCommentsPage = this.toTest
+                .getAllNotApprovedCommentsOrderedByDateDesc(this.pageable);
+
+        assertIterableEquals(expectedCommentsPage, actualCommentsPage);
+    }
+
+    @Test
+    void testApproveComment_ShouldSetApprovedToTrue() {
         CommentEntity comment = initComment();
 
         when(this.commentRepository.findById(comment.getId()))
@@ -65,11 +163,48 @@ public class CommentServiceTest {
 
         this.toTest.approveComment(comment.getId());
 
-        Assertions.assertTrue(comment.isApproved());
+        assertTrue(comment.isApproved());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4", "5"})
+    void testApproveComment_ShouldThrowException_WhenIdIsOutOfBounds(Long id) {
+        assertThrows(ObjectNotFoundException.class, () -> this.toTest.approveComment(id));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1", "2"})
+    void testDisapproveComment_ShouldDeleteComment(Long id) {
+        CommentEntity comment = initComment();
+
+        when(this.commentRepository.findTopByOrderByIdDesc()).thenReturn(comment);
+
+        this.toTest.disapproveComment(id);
+
+        verify(this.commentRepository, times(1)).deleteById(id);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4", "5", "6", "7"})
+    void testDisapproveComment_ShouldThrowException_WhenCommentIdIsOutOfBounds(Long id) {
+        CommentEntity comment = initComment();
+
+        when(this.commentRepository.findTopByOrderByIdDesc()).thenReturn(comment);
+
+        assertThrows(ObjectNotFoundException.class, () -> this.toTest.disapproveComment(id));
+    }
+
+    @Test
+    void testGetIdOfLastObjectInTable_ShouldReturnLastId() {
+        CommentEntity comment = initComment();
+
+        when(this.commentRepository.findTopByOrderByIdDesc()).thenReturn(comment);
+
+        assertEquals(3L, comment.getId());
     }
 
     private UserEntity initUser() {
-        UserEntity userEntity = new UserEntity()
+        return new UserEntity()
                 .setUsername("TestUsername")
                 .setFirstName("Test")
                 .setLastName("Test")
@@ -80,20 +215,16 @@ public class CommentServiceTest {
                 .setUserRoles(List.of(new UserRoleEntity(UserRoleEnum.ADMIN),
                         new UserRoleEntity(UserRoleEnum.MODERATOR),
                         new UserRoleEntity(UserRoleEnum.USER)));
-        this.userRepository.save(userEntity);
-        return userEntity;
     }
 
     private PostEntity initPost() {
-        PostEntity postEntity = new PostEntity()
+        return new PostEntity()
                 .setTitle("Title")
                 .setContent("Content")
                 .setCategory(PostCategoryEnum.PROBLEM)
                 .setViews(0)
                 .setCreatedOn(LocalDateTime.now())
                 .setApproved(true);
-        this.postRepository.save(postEntity);
-        return postEntity;
     }
 
     private CommentEntity initComment() {
@@ -103,8 +234,19 @@ public class CommentServiceTest {
                 .setCreatedOn(LocalDateTime.now())
                 .setPost(this.initPost())
                 .setAuthor(this.initUser());
-        commentEntity.setId(1L);
-        this.commentRepository.save(commentEntity);
+        commentEntity.setId(3L);
         return commentEntity;
+    }
+
+    private CommentDTO initCommentDto() {
+        return new CommentDTO(
+                1L, "Content", LocalDateTime.now(), "Username", "image:/url"
+        );
+    }
+
+    private AddCommentDTO initAddCommentDto() {
+        AddCommentDTO addCommentDTO = new AddCommentDTO();
+        addCommentDTO.setContent("Content");
+        return addCommentDTO;
     }
 }
