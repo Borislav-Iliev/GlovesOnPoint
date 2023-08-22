1f4ade4c607d0033ea677adb99edff018a32c629
Borislav Iliev <borislav.iliev2002@gmail.com>
2023-08-22 10:34:50 +0300

    Unit tests for Post Service.

--- src/test/java/bg/softuni/kickboxing/service/PostServiceTest.java
+++ src/test/java/bg/softuni/kickboxing/service/PostServiceTest.java
@@ -1,5 +1,9 @@
 package bg.softuni.kickboxing.service;
 
+import bg.softuni.kickboxing.model.dto.comment.CommentDTO;
+import bg.softuni.kickboxing.model.dto.post.AddPostDTO;
+import bg.softuni.kickboxing.model.dto.post.PostDTO;
+import bg.softuni.kickboxing.model.dto.post.PostDetailsDTO;
 import bg.softuni.kickboxing.model.entity.CommentEntity;
 import bg.softuni.kickboxing.model.entity.PostEntity;
 import bg.softuni.kickboxing.model.entity.UserEntity;
@@ -7,22 +11,28 @@ import bg.softuni.kickboxing.model.entity.UserRoleEntity;
 import bg.softuni.kickboxing.model.enums.PostCategoryEnum;
 import bg.softuni.kickboxing.model.enums.UserRoleEnum;
 import bg.softuni.kickboxing.model.exception.ObjectNotFoundException;
+import bg.softuni.kickboxing.model.user.GlovesOnPointUserDetails;
 import bg.softuni.kickboxing.repository.PostRepository;
 import bg.softuni.kickboxing.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
@@ -38,6 +48,123 @@ public class PostServiceTest {
 
     @Mock
     private ModelMapper mapper;
+
+    @Mock
+    private Pageable pageable;
+
+    @Mock
+    private GlovesOnPointUserDetails userDetails;
+
+    @ParameterizedTest
+    @CsvSource(value = {"ADMIN", "MODERATOR"})
+    void testAddPost_ShouldSetApprovedToTrueAndAddPost_WhenUserIsAdminOrModeratorAndCorrectDataIsPassed(String role) {
+        AddPostDTO addPostDTO = initAddPostDto();
+        PostEntity post = initPost();
+        when(this.mapper.map(addPostDTO, PostEntity.class))
+                .thenReturn(post);
+
+        UserEntity user = initUser();
+        when(this.userDetails.getUsername())
+                .thenReturn("Username");
+        when(this.userRepository.findByUsername(this.userDetails.getUsername()))
+                .thenReturn(Optional.of(user));
+
+        when(this.userDetails.getRole())
+                .thenReturn(role);
+
+        this.postService.addPost(addPostDTO, this.userDetails);
+
+        assertTrue(post.isApproved());
+        verify(this.postRepository, times(1)).save(post);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"USER"})
+    void testAddPost_ShouldSetApprovedToFalseAndAddPost_WhenUserIsANormalUserAndCorrectDataIsPassed(String role) {
+        AddPostDTO addPostDTO = initAddPostDto();
+        PostEntity post = initPost();
+        when(this.mapper.map(addPostDTO, PostEntity.class))
+                .thenReturn(post);
+
+        UserEntity user = initUser();
+        when(this.userDetails.getUsername())
+                .thenReturn("Username");
+        when(this.userRepository.findByUsername(this.userDetails.getUsername()))
+                .thenReturn(Optional.of(user));
+
+        when(this.userDetails.getRole())
+                .thenReturn(role);
+
+        this.postService.addPost(addPostDTO, this.userDetails);
+
+        assertFalse(post.isApproved());
+        verify(this.postRepository, times(1)).save(post);
+    }
+
+    @Test
+    void testGetAllApprovedPostsOrderedByDateDesc_ShouldReturnCorrectPageOfPosts() {
+        List<PostDTO> posts = List.of(initPostDto());
+        Page<PostDTO> expectedPostsPage = new PageImpl<>(posts);
+
+        when(this.postRepository.getAllApprovedPostsOrderedByCreatedOnDesc(this.pageable))
+                .thenReturn(expectedPostsPage);
+
+        Page<PostDTO> actualPostsPage = this.postService.getAllApprovedPostsOrderedByDateDesc(this.pageable);
+
+        assertIterableEquals(expectedPostsPage, actualPostsPage);
+    }
+
+    @Test
+    void testAddPost_ShouldThrowException_WhenUserHasAnyRoleAndUserCannotBeFound() {
+        AddPostDTO addPostDTO = initAddPostDto();
+
+        Executable executable = () -> this.postService.addPost(addPostDTO, this.userDetails);
+
+        assertThrows(RuntimeException.class, executable);
+    }
+
+    @Test
+    void testGetAllApprovedPostsByCategoryOrderedByCreatedOnDesc_ShouldReturnCorrectPageOfPosts() {
+        List<PostDTO> posts = List.of(initPostDto());
+        Page<PostDTO> expectedPostsPage = new PageImpl<>(posts);
+
+        when(this.postRepository.getAllApprovedPostsByCategoryOrderedByCreatedOnDesc(this.pageable, PostCategoryEnum.ARTICLE))
+                .thenReturn(expectedPostsPage);
+
+        Page<PostDTO> actualPostsPage = this.postService
+                .getAllApprovedPostsByCategoryOrderedByCreatedOnDesc(this.pageable, PostCategoryEnum.ARTICLE.name());
+
+        assertIterableEquals(expectedPostsPage, actualPostsPage);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"T", "Ti", "I"})
+    void testGetAllApprovedPostsWhereTitleLike_ShouldReturnCorrectPageOfPosts(String title) {
+        List<PostDTO> posts = List.of(initPostDto());
+        Page<PostDTO> expectedPostsPage = new PageImpl<>(posts);
+
+        when(this.postRepository.getAllApprovedPostsWhereTitleLike(this.pageable, title))
+                .thenReturn(expectedPostsPage);
+
+        Page<PostDTO> actualPostsPage = this.postService
+                .getAllApprovedPostsWhereTitleLike(this.pageable, title);
+
+        assertIterableEquals(expectedPostsPage, actualPostsPage);
+    }
+
+    @Test
+    void testGetAllNotApprovedPostsOrderedByDateDesc_ShouldReturnCorrectPageOfPosts() {
+        List<PostDTO> posts = List.of(initPostDto());
+        Page<PostDTO> expectedPostsPage = new PageImpl<>(posts);
+
+        when(this.postRepository.getAllNotApprovedPostsOrderedByCreatedOnDesc(this.pageable))
+                .thenReturn(expectedPostsPage);
+
+        Page<PostDTO> actualPostsPage = this.postService
+                .getAllNotApprovedPostsOrderedByDateDesc(this.pageable);
+
+        assertIterableEquals(expectedPostsPage, actualPostsPage);
+    }
 
     @ParameterizedTest
     @CsvSource(value = {"1", "2", "3", "4"})
@@ -65,6 +192,120 @@ public class PostServiceTest {
     @CsvSource(value = {"-1", "-2", "-3", "-4"})
     void testFindById_ShouldThrowException_WhenIncorrectPostIdIsPassed(Long id) {
         assertThrows(ObjectNotFoundException.class, () -> this.postService.findById(id));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1", "2", "3", "4"})
+    void testGetPostDetailsById_ShouldReturnCorrectPost_WhenValidPostIdIsPassed(Long id) {
+        PostEntity post = initPost();
+        when(this.postRepository.findById(id))
+                .thenReturn(Optional.of(post));
+
+        PostDetailsDTO postDetailsDto = initPostDetailsDto();
+        when(this.mapper.map(post, PostDetailsDTO.class))
+                .thenReturn(postDetailsDto);
+
+        PostDetailsDTO actualPostDetailsDto = this.postService.getPostDetailsById(id);
+
+        assertEquals(postDetailsDto.getId(), actualPostDetailsDto.getId());
+        assertEquals(postDetailsDto.getTitle(), actualPostDetailsDto.getTitle());
+        assertEquals(postDetailsDto.getContent(), actualPostDetailsDto.getContent());
+        assertEquals(postDetailsDto.getCategory(), actualPostDetailsDto.getCategory());
+        assertEquals(postDetailsDto.getViews(), actualPostDetailsDto.getViews());
+        assertEquals(postDetailsDto.getCreatedOn(), actualPostDetailsDto.getCreatedOn());
+        assertIterableEquals(postDetailsDto.getComments(), actualPostDetailsDto.getComments());
+        assertEquals(postDetailsDto.getAuthorUsername(), actualPostDetailsDto.getAuthorUsername());
+        assertEquals(postDetailsDto.getAuthorImageUrl(), actualPostDetailsDto.getAuthorImageUrl());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"-1", "-2", "-3", "-4"})
+    void testGetPostDetailsById_ShouldThrowException_WhenInvalidPostIdIsPassed(Long id) {
+        Executable executable = () -> this.postService.getPostDetailsById(id);
+
+        assertThrows(ObjectNotFoundException.class, executable);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1", "2", "3", "4"})
+    void testIncreaseViewsCount_ShouldIncreaseViewsCountOfPost_WhenValidPostIdIsPassed(Long id) {
+        PostEntity post = initPost();
+        when(this.postRepository.findById(id))
+                .thenReturn(Optional.of(post));
+
+        this.postService.increaseViewsCount(id);
+
+        verify(this.postRepository, times(1)).save(post);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"-1", "-2", "-3", "-4"})
+    void testIncreaseViewsCount_ShouldThrowException_WhenInvalidPostIdIsPassed(Long id) {
+        when(this.postRepository.findById(id))
+                .thenThrow(RuntimeException.class);
+
+        Executable executable = () -> this.postService.increaseViewsCount(id);
+
+        assertThrows(RuntimeException.class, executable);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1", "2", "3", "4"})
+    void testApprovePost_ShouldSetApprovedToTrueAndSavePost_WhenValidPostIdIsPassed(Long id) {
+        PostEntity post = initPost();
+        when(this.postRepository.findById(id))
+                .thenReturn(Optional.of(post));
+
+        this.postService.approvePost(id);
+
+        assertTrue(post.isApproved());
+        verify(this.postRepository, times(1)).save(post);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"-1", "-2", "-3", "-4"})
+    void testApprovePost_ShouldThrowException_WhenInvalidPostIdIsPassed(Long id) {
+        Executable executable = () -> this.postService.approvePost(id);
+        assertThrows(ObjectNotFoundException.class, executable);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1", "2", "3", "4"})
+    void testDisapprovePost_ShouldDeletePost_WhenValidPostIdIsPassed(Long id) {
+        PostEntity post = initPost();
+        post.setId(id);
+        when(this.postRepository.findTopByOrderByIdDesc())
+                .thenReturn(post);
+
+        this.postService.disapprovePost(id);
+
+        verify(this.postRepository, times(1)).deleteById(id);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"3", "4"})
+    void testDisapprovePost_ShouldThrowException_WhenInvalidPostIdIsPassed(Long id) {
+        PostEntity post = initPost();
+        post.setId(2L);
+        when(this.postRepository.findTopByOrderByIdDesc())
+                .thenReturn(post);
+
+        Executable executable = () -> this.postService.disapprovePost(id);
+
+        assertThrows(ObjectNotFoundException.class, executable);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1", "2", "3", "4"})
+    void testGetIdOfLastObjectInTable_ShouldReturnIdOfLastObject(Long id) {
+        PostEntity post = initPost();
+        post.setId(id);
+        when(this.postRepository.findTopByOrderByIdDesc())
+                .thenReturn(post);
+
+        Long actualLastId = this.postService.getIdOfLastObjectInTable();
+
+        assertEquals(post.getId(), actualLastId);
     }
 
     private PostEntity initPost() {
@@ -100,5 +341,33 @@ public class PostServiceTest {
                 .setUserRoles(List.of(new UserRoleEntity(UserRoleEnum.ADMIN),
                         new UserRoleEntity(UserRoleEnum.MODERATOR),
                         new UserRoleEntity(UserRoleEnum.USER)));
+    }
+
+    private AddPostDTO initAddPostDto() {
+        AddPostDTO addPostDTO = new AddPostDTO();
+        addPostDTO.setTitle("Title");
+        addPostDTO.setContent("Content");
+        addPostDTO.setCategory(PostCategoryEnum.ARTICLE);
+        return addPostDTO;
+    }
+
+    private PostDTO initPostDto() {
+        return new PostDTO(
+                1L, "Username", "image:/url", 1L,
+                "Title", PostCategoryEnum.ARTICLE, 1, LocalDateTime.now()
+        );
+    }
+
+    private PostDetailsDTO initPostDetailsDto() {
+        return new PostDetailsDTO(
+                1L, "Title", "Content", PostCategoryEnum.ARTICLE, 1,
+                LocalDateTime.now(), List.of(initCommentDto()), "Username", "image:/url"
+        );
+    }
+
+    private CommentDTO initCommentDto() {
+        return new CommentDTO(
+                1L, "Content", LocalDateTime.now(), "Username", "image:/url"
+        );
     }
 }
